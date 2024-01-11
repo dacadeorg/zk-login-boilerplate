80936a83cac99929b49922272e25dfe094a5b746
emmanuel <Emmanuelsumeh@gmail.com>
2024-01-11 10:19:49 +0100

    fix:  boilerplate setup

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Callback from './Callback';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <Routes>
-      <Route path="/notes" element={<App />} />
-      <Route path="/" element={<Callback />} />
-    </Routes>
-  </Router>
+ <h1>Sui ZK Login</h1>
 );
 
 reportWebVitals();
